7fc3e894521b9bd2e4d4fed76289789bed7bde1f
Dreamwalker <aristojeff@gmail.com>
2018-11-22 12:00:37 +0900

    submei

--- app/src/main/java/com/dreamwalkers/elab_yang/mmk/activity/HomeActivity.java
+++ app/src/main/java/com/dreamwalkers/elab_yang/mmk/activity/HomeActivity.java
@@ -1,6 +1,5 @@
 package com.dreamwalkers.elab_yang.mmk.activity;
 
-import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.Intent;
 import android.content.res.TypedArray;
@@ -27,13 +26,11 @@ import com.dreamwalkers.elab_yang.mmk.activity.fragment.FragmentA;
 import com.dreamwalkers.elab_yang.mmk.activity.fragment.FragmentB;
 import com.dreamwalkers.elab_yang.mmk.activity.fragment.FragmentC;
 import com.dreamwalkers.elab_yang.mmk.activity.fragment.FragmentD;
-import com.dreamwalkers.elab_yang.mmk.activity.sync.needle.v1.SelectDrugFirstActivity2;
+import com.dreamwalkers.elab_yang.mmk.activity.select.SelectDrugActivity;
 import com.dreamwalkers.elab_yang.mmk.menu.DrawerAdapter;
 import com.dreamwalkers.elab_yang.mmk.menu.DrawerItem;
 import com.dreamwalkers.elab_yang.mmk.menu.SimpleItem;
 import com.dreamwalkers.elab_yang.mmk.menu.SpaceItem;
-
-import org.jetbrains.anko.AlertDialogBuilder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -124,6 +121,7 @@ public class HomeActivity extends AppCompatActivity implements DrawerAdapter.OnI
 
             case POS_ACCOUNT:
                 Log.d(TAG, "onItemSelected: POS_ACCOUNT");
+                startActivity(new Intent(this, SelectDrugActivity.class));
                 break;
 
             // TODO: 2018-11-19 인슐린 설정은 여기서 ㄱㄱㄱㄱㄱㄱㄱ
--- app/src/main/java/com/dreamwalkers/elab_yang/mmk/activity/select/SelectDrugActivity.kt
+++ app/src/main/java/com/dreamwalkers/elab_yang/mmk/activity/select/SelectDrugActivity.kt
@@ -1,5 +1,6 @@
 package com.dreamwalkers.elab_yang.mmk.activity.select
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
@@ -13,6 +14,9 @@ import com.dreamwalkers.elab_yang.mmk.adapter.drugselect.LeftAdapter
 import com.dreamwalkers.elab_yang.mmk.adapter.drugselect.RightAdapter
 import com.dreamwalkers.elab_yang.mmk.model.drugselect.Track
 import com.dreamwalkers.elab_yang.mmk.model.drugselect.TrackList
+import org.json.JSONArray
+import org.json.JSONObject
+import java.util.logging.Logger
 
 class SelectDrugActivity : AppCompatActivity() {
 
@@ -66,6 +70,38 @@ class SelectDrugActivity : AppCompatActivity() {
                 } else {
                     msg = resources.getQuantityString(R.plurals.you_selected_x_songs, selectedCount, selectedCount)
                     mMultiSelect!!.showSelectedPage()
+                    for (x in 0 until items.size) {
+                        Logger.getLogger(SelectDrugActivity::class.java.name).warning("${items[x].trackName} --> ${items[x].artist}")
+                    }
+                    //todo 받아들여진 데이터 처리 --> 액티비티간 넘기기
+
+                    val jsonObject = JSONObject()
+                    val jsonArray = JSONArray()
+
+                    for (x in 0 until items.size) {
+                        var job = JSONObject()
+                        job.put("name", items[x].trackName)
+                        job.put("kind", items[x].artist)
+                        jsonArray.put(job)
+                    }
+
+                    jsonObject.put("count", items.size)
+                    jsonObject.put("items", jsonArray)
+
+                    Logger.getLogger(SelectDrugActivity::class.java.name).warning(jsonObject.toString())
+
+                    val resultIntent = Intent(this, SelectDrugActivity::class.java)
+
+                    val receiveDate = JSONObject(jsonObject.toString())
+                    Logger.getLogger(SelectDrugActivity::class.java.name).warning(receiveDate.getString("count"))
+
+                    val receivedJsonArray = receiveDate.getJSONArray("items")
+
+                    for (x in 0 until receivedJsonArray.length()){
+                        Logger.getLogger(SelectDrugActivity::class.java.name).warning( receivedJsonArray.getJSONObject(x).getString("name")   )
+                    }
+
+
                 }
                 Snackbar.make(toolbar, msg, Snackbar.LENGTH_LONG).show()
                 true
